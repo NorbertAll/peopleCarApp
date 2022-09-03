19bc7da154042225e7dbdd3521329fd873cb3603
NorbertAll <norbert_gutkowski@interia.pl>
2022-09-02 21:21:31 -1200

    calculate Dissel Usage For Distance

--- carPeopleApi/src/main.rs
+++ carPeopleApi/src/main.rs
@@ -1,11 +1,42 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, env};
 
 use warp::Filter;
 
 async fn calculate_dissel_usage_for_distance(
     param: HashMap<String, String>,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(format!("calculateDisselUsageForDistance {:#?}", param))
+    let mut distance = 0.0;
+    let mut year_of_production = 0;
+    let mut fuel_usage = 0.0;
+    let mut fuel_consumotion = 0.0;
+    for (key, value) in &param {
+        if key == "distance" {
+            let my_string = value.to_string();
+            let dist = my_string.parse::<f64>().unwrap_or(0.0);
+            distance = dist;
+        }
+        if key == "yearOfProduction" {
+            let my_stringx = value.to_string(); // `parse()` works with `&str` and `String`!
+            let yer = my_stringx.parse::<i32>().unwrap_or(0);
+            year_of_production = yer;
+        }
+        if key == "fuelUsagePer100KM" {
+            let my_stringxx = value.to_string(); // `parse()` works with `&str` and `String`!
+            let us = my_stringxx.parse::<f64>().unwrap_or(0.0);
+            fuel_usage = us;
+        }
+    }
+    if distance > 0.0 && fuel_usage > 0.0 {
+        fuel_consumotion = (distance / 100.0) * fuel_usage;
+    }
+    if fuel_consumotion <= 0.0 {
+        Ok(format!("Error"))
+    } else {
+        Ok(format!(
+            "{{\"calculateDisselUsageForDistance\": {}}}",
+            fuel_consumotion
+        ))
+    }
 }
 
 async fn probability_of_unit_injector_fail(
